cd926f45ed9d71a5333e748c3924d56f4a070966
Gavin Wakeley <Gavin.Wakeley@bjss.com>
2020-08-24 12:09:59 +0100

    Switched keyvault lock to delete lock

--- generateKeyVault/keyvault_management_locks.tf
+++ generateKeyVault/keyvault_management_locks.tf
@@ -1,6 +1,6 @@
 resource "azurerm_management_lock" "keyvault_lock" {
   name       = "write-lock-keyvault"
   scope      = azurerm_key_vault.keyvault.id
-  lock_level = "ReadOnly"
-  notes      = "To prevent key vault being changed"
+  lock_level = "Delete"
+  notes      = "To prevent key vault being deleted"
 }
