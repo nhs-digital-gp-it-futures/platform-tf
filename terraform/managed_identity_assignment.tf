1a21e3bdf77c93b23fcc0d2fe826cfcc79003a9e
Gavin Wakeley <Gavin.Wakeley@bjss.com>
2020-09-16 15:21:19 +0100

    Test Build full environment

--- terraform/managed_identity_assignment.tf
+++ terraform/managed_identity_assignment.tf
@@ -10,3 +10,9 @@ resource "azurerm_role_assignment" "managed_AGRG_Access" {
   role_definition_name = "Reader"
   principal_id         = azurerm_user_assigned_identity.managed_id.principal_id
 }
+
+resource "azurerm_role_assignment" "managed_AAD_Access" {
+  scope                = azurerm_user_assigned_identity.managed_id.id
+  role_definition_name = "Managed Identity Operator"
+  principal_id         = azurerm_user_assigned_identity.managed_id.principal_id
+}
