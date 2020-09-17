d43db58a8d468e7b3b1957661f1df1cee0225135
Gavin Wakeley <Gavin.Wakeley@bjss.com>
2020-09-17 11:25:34 +0100

    Added working dependencies

--- terraform/application_gateways_dev.tf
+++ terraform/application_gateways_dev.tf
@@ -182,4 +182,22 @@ resource "azurerm_application_gateway" "AppGate" {
   depends_on = [
     azurerm_network_security_rule.Azure
   ]
+
+  lifecycle {
+    # AGIC owns most app gateway settings, so we should ignore differences
+    ignore_changes = [
+      #gateway_ip_configuration,
+      request_routing_rule, 
+      http_listener, 
+      backend_http_settings, 
+      #frontend_ip_configuration, 
+      frontend_port,
+      backend_address_pool,
+      probe,
+      redirect_configuration,      
+      #url_path_map,     
+      tags, # AGIC adds tags which need to be ignored. Can't seem to ignore the individual tags
+      #ssl_certificate # see issue above
+    ]
+  }
 }
