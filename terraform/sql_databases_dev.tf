cd0e9723cfa326526c6a3ecd910d568bb978d0cb
Gavin Wakeley <Gavin.Wakeley@bjss.com>
2020-10-08 13:57:41 +0100

    Set dummy db names

--- terraform/sql_databases_dev.tf
+++ terraform/sql_databases_dev.tf
@@ -2,7 +2,7 @@ resource "azurerm_sql_database" "sql-bapi-pri-dev" {
   count                            = local.shortenv != "test" && local.shortenv != "prod" ? 1 : 0 
 
 #  name                             = "${var.project}-${var.environment}-db-bapi-private"
-  name                             = "bc-${var.environment}-db-bapi"
+  name                             = "xbc-${var.environment}-db-bapi"
   resource_group_name              = azurerm_resource_group.sql-pri.name
   location                         = var.region
   server_name                      = azurerm_sql_server.sql-pri.name
@@ -36,7 +36,7 @@ resource "azurerm_sql_database" "sql-isapi-dev" {
   count                            = local.shortenv != "test" && local.shortenv != "prod" ? 1 : 0 
 
   #name                             = "${var.project}-${var.environment}-db-isapi"
-  name                             = "bc-${var.environment}-db-isapi"
+  name                             = "xbc-${var.environment}-db-isapi"
   resource_group_name              = azurerm_resource_group.sql-pri.name
   location                         = var.region
   server_name                      = azurerm_sql_server.sql-pri.name
@@ -53,7 +53,7 @@ resource "azurerm_sql_database" "sql-ordapi-dev" {
   count                            = local.shortenv != "test" && local.shortenv != "prod" ? 1 : 0 
 
   #name                             = "${var.project}-${var.environment}-db-ordapi"
-  name                             = "bc-${var.environment}-db-ordapi"
+  name                             = "xbc-${var.environment}-db-ordapi"
   resource_group_name              = azurerm_resource_group.sql-pri.name
   location                         = var.region
   server_name                      = azurerm_sql_server.sql-pri.name
