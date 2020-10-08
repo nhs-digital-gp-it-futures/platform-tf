817e627f6a7d7c5ca0c7f156a9a32e16f04b5aac
Gavin Wakeley <Gavin.Wakeley@bjss.com>
2020-10-08 14:13:18 +0100

    Corrected key vault name

--- terraform/sql_databases_dev.tf
+++ terraform/sql_databases_dev.tf
@@ -1,67 +1,67 @@
-resource "azurerm_sql_database" "sql-bapi-pri-dev" {
-  count                            = local.shortenv != "test" && local.shortenv != "prod" ? 1 : 0 
+# resource "azurerm_sql_database" "sql-bapi-pri-dev" {
+#   count                            = local.shortenv != "test" && local.shortenv != "prod" ? 1 : 0 
 
-#  name                             = "${var.project}-${var.environment}-db-bapi-private"
-  name                             = "xbc-${var.environment}-db-bapi"
-  resource_group_name              = azurerm_resource_group.sql-pri.name
-  location                         = var.region
-  server_name                      = azurerm_sql_server.sql-pri.name
-  collation                        = local.sql_collation
-  edition                          = local.sql_edition
-  requested_service_objective_name = local.sql_size
+# #  name                             = "${var.project}-${var.environment}-db-bapi-private"
+#   name                             = "xbc-${var.environment}-db-bapi"
+#   resource_group_name              = azurerm_resource_group.sql-pri.name
+#   location                         = var.region
+#   server_name                      = azurerm_sql_server.sql-pri.name
+#   collation                        = local.sql_collation
+#   edition                          = local.sql_edition
+#   requested_service_objective_name = local.sql_size
 
-  tags = {
-    environment                    = var.environment
-  }
-}
+#   tags = {
+#     environment                    = var.environment
+#   }
+# }
 
-resource "azurerm_sql_database" "sql-bapi-pub-live" {
-  count                            = local.shortenv == "test" && local.shortenv == "prod" ? 1 : 0 
+# resource "azurerm_sql_database" "sql-bapi-pub-live" {
+#   count                            = local.shortenv == "test" && local.shortenv == "prod" ? 1 : 0 
 
-  name                             = "${var.project}-${var.environment}-db-bapi-public"
+#   name                             = "${var.project}-${var.environment}-db-bapi-public"
   
-  resource_group_name              = azurerm_resource_group.sql-pri.name
-  location                         = var.region
-  server_name                      = azurerm_sql_server.sql-pri.name
-  collation                        = local.sql_collation
-  edition                          = local.sql_edition
-  requested_service_objective_name = local.sql_size
+#   resource_group_name              = azurerm_resource_group.sql-pri.name
+#   location                         = var.region
+#   server_name                      = azurerm_sql_server.sql-pri.name
+#   collation                        = local.sql_collation
+#   edition                          = local.sql_edition
+#   requested_service_objective_name = local.sql_size
 
-  tags = {
-    environment                    = var.environment
-  }
-}
+#   tags = {
+#     environment                    = var.environment
+#   }
+# }
 
-resource "azurerm_sql_database" "sql-isapi-dev" {
-  count                            = local.shortenv != "test" && local.shortenv != "prod" ? 1 : 0 
+# resource "azurerm_sql_database" "sql-isapi-dev" {
+#   count                            = local.shortenv != "test" && local.shortenv != "prod" ? 1 : 0 
 
-  #name                             = "${var.project}-${var.environment}-db-isapi"
-  name                             = "xbc-${var.environment}-db-isapi"
-  resource_group_name              = azurerm_resource_group.sql-pri.name
-  location                         = var.region
-  server_name                      = azurerm_sql_server.sql-pri.name
-  collation                        = local.sql_collation
-  edition                          = local.sql_edition
-  requested_service_objective_name = local.sql_size
+#   #name                             = "${var.project}-${var.environment}-db-isapi"
+#   name                             = "xbc-${var.environment}-db-isapi"
+#   resource_group_name              = azurerm_resource_group.sql-pri.name
+#   location                         = var.region
+#   server_name                      = azurerm_sql_server.sql-pri.name
+#   collation                        = local.sql_collation
+#   edition                          = local.sql_edition
+#   requested_service_objective_name = local.sql_size
 
-  tags = {
-    environment                    = var.environment
-  }
-}
+#   tags = {
+#     environment                    = var.environment
+#   }
+# }
 
-resource "azurerm_sql_database" "sql-ordapi-dev" {
-  count                            = local.shortenv != "test" && local.shortenv != "prod" ? 1 : 0 
+# resource "azurerm_sql_database" "sql-ordapi-dev" {
+#   count                            = local.shortenv != "test" && local.shortenv != "prod" ? 1 : 0 
 
-  #name                             = "${var.project}-${var.environment}-db-ordapi"
-  name                             = "xbc-${var.environment}-db-ordapi"
-  resource_group_name              = azurerm_resource_group.sql-pri.name
-  location                         = var.region
-  server_name                      = azurerm_sql_server.sql-pri.name
-  collation                        = local.sql_collation
-  edition                          = local.sql_edition
-  requested_service_objective_name = local.sql_size
+#   #name                             = "${var.project}-${var.environment}-db-ordapi"
+#   name                             = "xbc-${var.environment}-db-ordapi"
+#   resource_group_name              = azurerm_resource_group.sql-pri.name
+#   location                         = var.region
+#   server_name                      = azurerm_sql_server.sql-pri.name
+#   collation                        = local.sql_collation
+#   edition                          = local.sql_edition
+#   requested_service_objective_name = local.sql_size
 
-  tags = {
-    environment                    = var.environment
-  }
-}
+#   tags = {
+#     environment                    = var.environment
+#   }
+# }
