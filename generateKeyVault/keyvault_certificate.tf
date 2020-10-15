bd263872b6098edb5048a9184a105a8e28e0354f
Gavin Wakeley <Gavin.Wakeley@bjss.com>
2020-10-15 11:17:17 +0100

    Generate certificate and use in helm

---
+++ generateKeyVault/keyvault_certificate.tf
@@ -0,0 +1,79 @@
+# resource "azurerm_key_vault_secret" "kv-sqluser" {
+#   name         = "${var.pjtcode}${local.shortEnv}sqladminusername"
+#   value        = var.kv_sqlusername
+#   content_type = "${var.project}-SQL-Username"
+#   key_vault_id = azurerm_key_vault.keyvault.id
+  
+#   tags = {
+#     environment = var.environment
+#   }
+
+#   depends_on = [
+#     azurerm_key_vault_access_policy.keyvault_devops_access,
+#     azurerm_key_vault_access_policy.keyvault_access,
+#     azurerm_key_vault_access_policy.keyvault_current_access
+#   ]
+# }
+
+# resource "random_password" "password1" {
+#   length = 16
+#   special = true
+#   override_special = "$_%@"
+# }
+# #random_password.password1.result
+
+resource "azurerm_key_vault_certificate" "kv-azurecert" {
+  name         = "buyingcatalogue${var.environment}"
+  key_vault_id = azurerm_key_vault.keyvault.id
+
+  certificate_policy {
+    issuer_parameters {
+      name = "Self"
+    }
+
+    key_properties {
+      exportable = false
+      key_size   = 2048
+      key_type   = "RSA"
+      reuse_key  = true
+    }
+
+    lifetime_action {
+      action {
+        action_type = "AutoRenew"
+      }
+
+      trigger {
+        days_before_expiry = 30
+      }
+    }
+
+    secret_properties {
+      content_type = "application/x-pkcs12"
+    }
+
+    x509_certificate_properties {
+      #Server Authentication = 1.3.6.1.5.5.7.3.1
+      #Client Authentication = 1.3.6.1.5.5.7.3.2
+      extended_key_usage = ["1.3.6.1.5.5.7.3.1", "1.3.6.1.5.5.7.3.2"]
+
+      key_usage = [
+        "cRLSign",
+        "dataEncipherment",
+        "digitalSignature",
+        "keyAgreement",
+        "keyCertSign",
+        "keyEncipherment",
+      ]
+
+      subject_alternative_names {
+        dns_names = [
+            "buyingcatalogue${var.environment}.${var.region}.cloudapp.azure.com"
+            ]
+      }
+
+      subject            = "CN = buyingcatalogue${var.environment}.${var.region}.cloudapp.azure.com"
+      validity_in_months = 12
+    }
+  }
+}
