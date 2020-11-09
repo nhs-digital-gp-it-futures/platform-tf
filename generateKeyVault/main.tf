9ce9209a6e3104d25805dca6726f32bad4145ed0
Gavin Wakeley <Gavin.Wakeley@bjss.com>
2020-11-09 15:43:52 +0000

    Updated azurerm versions to latest

--- generateKeyVault/main.tf
+++ generateKeyVault/main.tf
@@ -5,7 +5,7 @@ terraform {
 }
 
 provider "azurerm" {
-  version                = "2.10.0"
+  version                = "2.35.0"
 
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
