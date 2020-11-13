275ea59ffd4fe64531b7ed238f04d8c3a6cf6464
Gavin Wakeley <Gavin.Wakeley@bjss.com>
2020-11-13 15:42:14 +0000

    Add backend node config

--- terraform/kubernetes_variables.tf
+++ terraform/kubernetes_variables.tf
@@ -1,4 +1,7 @@
 locals {
-    kv_aksversion = "1.18.10"
-    kv_aksvmsize  = "Standard_B4ms"
+    kv_aksversion   = "1.18.10"
+    kv_aksvmsize    = "Standard_B4ms"
+    kv_aksinitnodes = 2
+    kv_aksmaxnodes = 2
+    kv_aksminnodes = 1
 }
