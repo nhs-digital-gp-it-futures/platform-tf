ed7c8a0a035526171398bf3bfb3cf5667e6320be
Gavin.Wakeley <gavin.wakeley@bjss.com>
2021-01-20 11:01:50 +0000

    Update rancher URL

--- terraform/dns_record_dyn.tf
+++ terraform/dns_record_dyn.tf
@@ -15,7 +15,7 @@ resource "azurerm_dns_cname_record" "dyn_cname" {
 
 resource "azurerm_dns_cname_record" "rancher_cname" {
     count               = local.shortenv != "development" && local.shortenv != "preprod" && local.shortenv != "production" ? 1 : 0
-    name                = "rancher-${var.environment}"
+    name                = "rancher.${var.environment}"
     zone_name           = data.azurerm_dns_zone.dyn_zone[0].name
     resource_group_name = "gpitfutures-dynamic-rg"
     ttl                 = 60
