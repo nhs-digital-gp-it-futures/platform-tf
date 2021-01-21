ae24ac17ea97e85e30e2fc811cf2f584f95ffff0
Gavin.Wakeley <gavin.wakeley@bjss.com>
2021-01-21 13:49:28 +0000

    Add switch into local

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,8 +1,12 @@
 locals {
-  kv_id = "/subscriptions/${var.subscription_id}/resourceGroups/${var.project}-${var.keyvaultrg}/providers/Microsoft.KeyVault/vaults/${var.keyvault}"
+  shortenv = replace(var.environment, "-", "")
+  # Top Level Environment Calculator
   liveEnv = local.shortenv == "production" ? "prod" : "test"
   coreEnv = local.shortenv != "preprod" && local.shortenv != "production" ? "dev" : local.liveEnv
+  # KeyVault IDs
+  kv_id = "/subscriptions/${var.subscription_id}/resourceGroups/${var.project}-${var.keyvaultrg}/providers/Microsoft.KeyVault/vaults/${var.keyvault}"
   corekv_id = "/subscriptions/${var.subscription_id}/resourceGroups/${var.project}-${local.coreEnv}-rg-kv/providers/Microsoft.KeyVault/vaults/${var.project}-${local.coreEnv}-core-kv"
-  shortenv = replace(var.environment, "-", "")
-  envURL = replace(data.azurerm_key_vault_secret.coreurl.value, "${var.environment}.","")
+  # Rancher URL calculator
+  rancherEnv = local.shortenv != "preprod" && local.shortenv != "production" ? var.environment : local.liveEnv
+  envURL = replace(data.azurerm_key_vault_secret.coreurl.value, "${local.rancherEnv}.","")
 }
