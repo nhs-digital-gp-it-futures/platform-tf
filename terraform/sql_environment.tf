c40d5f91e299741b2e4ce22cc77d85b501108855
Gavin.Wakeley <gavin.wakeley@bjss.com>
2021-02-09 16:06:27 +0000

    Fix sql config

--- terraform/sql_environment.tf
+++ terraform/sql_environment.tf
@@ -31,7 +31,7 @@ module "sql_server_sec" {
   sqladmins             = data.azurerm_key_vault_secret.sqladmins.value
   bjssvpn               = data.azurerm_key_vault_secret.bjssvpn.value
   mastekvpn             = "1.1.1.1"
-  aks_subnet_id         = azurerm_subnet.aks.id
+  aks_subnet_id         = ""
 }
 
 module "sql_databases" {
@@ -40,15 +40,15 @@ module "sql_databases" {
   count                 = local.shortenv == "preprod" || local.shortenv == "production" ? 1 : 0 
   
   environment           = var.environment
-  region                = local.sql_region2
+  region                = var.region
   project               = var.project
   rg_name               = azurerm_resource_group.sql-pri.name
-  sqlsvr_name           = "${var.project}-${var.environment}-sql-sec"
+  sqlsvr_name           = "${var.project}-${var.environment}-sql-pri"
   db_name               = local.shortenv == "production" ? "bc-buyingcatalogue" : "bc-bc-${var.environment}"
   region_replica        = local.sql_region2
   rg_replica_name       = azurerm_resource_group.sql-sec[0].name
   sqlsvr_replica_name   = "${var.project}-${var.environment}-sql-sec"
   sql_collation         = "SQL_Latin1_General_CP1_CI_AS"
   sql_edition           = "Standard"
-  sql_size              = "S1"
+  sql_size              = "S0"
 } 
